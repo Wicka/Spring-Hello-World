ddde7e37120d911180694f7304a314e4069d9708
Wicka <mesaes@gmail.com>
2020-07-07 12:11:43 +0200

    Identificacion de la clase Inicial y cambio de nombre por Aplicacion.java

--- src/main/java/es/ejercicio/spring/ServletInitializer.java
+++ src/main/java/es/ejercicio/spring/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringGettingStartedApplication.class);
+		return application.sources(Aplicacion.class);
 	}
 
 }

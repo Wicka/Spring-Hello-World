d8f41cd77a9aed355eb68793f8e0e39d2c8a108f
Wicka <mesaes@gmail.com>
2020-07-07 13:05:11 +0200

    Interaccion con los documentos html

--- src/main/java/es/ejercicio/spring/SaludoController.java
+++ src/main/java/es/ejercicio/spring/SaludoController.java
@@ -2,22 +2,32 @@ package es.ejercicio.spring;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class SaludoController {
 	
 	private static final String template ="Hello, %s";
 	private final AtomicLong iCont = new AtomicLong();
 	
-	@GetMapping ("/")
+	/*@GetMapping ("/")
 	public Saludo saludo(@RequestParam(value="name", defaultValue="WORLD")String name) {
 		return new Saludo(iCont.incrementAndGet(), String.format(template, name));
 	}
+	*/
 	
-	
+	 @RequestMapping("/hola")
+	    public String hola(@RequestParam(value="nombre", required=false, defaultValue="Mundo") String nombre, Model model){
+	       
+		 model.addAttribute("nombre", nombre);
+	     
+		 return "hola";
+	    }
 	
 
 }

d77993e18cdf71b63acda4111b4bd42ba4e37952
Wicka <mesaes@gmail.com>
2020-07-07 19:53:14 +0200

    tercera manera

---
+++ src/test/java/es/ejercicio/spring/AplicacionTests.java
@@ -0,0 +1,13 @@
+package es.ejercicio.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

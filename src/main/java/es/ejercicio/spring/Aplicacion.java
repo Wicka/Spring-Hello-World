3c75c9e4073ac731ce8c1fb1f1d7423d8716799d
Wicka <mesaes@gmail.com>
2020-07-08 09:40:37 +0200

    Corregida la tercera opcion de pasar parametro mediante JQuery $Ajax

---
+++ src/main/java/es/ejercicio/spring/Aplicacion.java
@@ -0,0 +1,15 @@
+package es.ejercicio.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aplicacion {
+
+	//DESDE ESTA CLASE MAIN ES DONDE SE LANZA EL PROYECTO SPRING
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Aplicacion.class, args);
+	}
+
+}
